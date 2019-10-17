144af3afb85c4e41e6e1a6765b6f9adf9f25375d
ggeorge <gmgeorge1993@gmail.com>
2019-10-16 19:29:38 -0500

    added firebase

---
+++ assets/logic.js
@@ -0,0 +1,14 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyB7XtWA6PQXqUuaBE6zFiRt75nBXAPy7Zw",
+    authDomain: "traintime-ea8e5.firebaseapp.com",
+    databaseURL: "https://traintime-ea8e5.firebaseio.com",
+    projectId: "traintime-ea8e5",
+    storageBucket: "traintime-ea8e5.appspot.com",
+    messagingSenderId: "491082082642",
+    appId: "1:491082082642:web:930f45efc7f9bee9af728b"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  
