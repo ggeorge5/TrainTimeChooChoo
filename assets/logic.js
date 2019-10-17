d81b51cbceb5a8ee741571151766ab7130c8810f
ggeorge <gmgeorge1993@gmail.com>
2019-10-17 18:18:39 -0500

    added some logic. STill not working.

--- assets/logic.js
+++ assets/logic.js
@@ -9,27 +9,51 @@
     appId: "1:491082082642:web:930f45efc7f9bee9af728b"
   };
   // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(config);
 
   var trainDatabase = firebase.database();
 
   //adding train button to collect then store info
-  $(document).on("click", '#addTrain', function(event) {
+  $("addTrain").on("click", function(event) {
       event.preventDefault();
       var trainName = $("#train-name-input").val().trim();
-      var destination = $("#destination-input").val().trim();
+      var trainDestination = $("#destination-input").val().trim();
       var firstTrain = moment($("#time-input").val().trim(), "HH:mm").format("military time");
       var frequency = $("#frequency-input").val().trim();
 
-      console.log(firstTrain);
 
-      trainInputs = {
-          trainName: trainName,
-          destination: destination,
-          firstTrain: firstTrain,
-          frequency: frequency
-      }
+      var trainInputs = {
+          Name: trainName,
+          destination: trainDestination,
+          start: firstTrain,
+          time: frequency
+      };
 
       trainDatabase.ref().push(trainInputs)
+
+      alert("Train has been scheduled");
+
+      $("#train-name-input").val("");
+      $("#destination-input").val("");
+      $("#time-input").val("");
+      $("#frequency-input").val("");
   });
+
+  database.ref().on("child_added", function(childSnapshot) {
+    console.log(childSnapshot.val());
+
+    var trnName = childSnapshot.val().name;
+    var trnDestination = childSnapshot.val().role;
+    var trnTime = childSnapshot.val().start;
+    var trnFrequency = childSnapshot.val().rate;
+
+    console.log(trnName);
+    console.log(trnDestination);
+    console.log(trnTime);
+    console.log(trnFrequency);
+
+    var trainStartPretty = moment.unix(trnTime).format("HH:mm");
+
+    var trnMinutes = moment().diff(moment(trnTime).format)
+  })
 
